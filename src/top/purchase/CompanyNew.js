49fb3a9a95cb3dd185c01e30972a53b58e444cfd
gukhyeonnam <sosiq@kakao.com>
2022-12-21 14:33:55 +0900

    first commit

--- src/top/purchase/CompanyNew.js
+++ src/top/purchase/CompanyNew.js
@@ -1,22 +1,33 @@
 import React from "react";
+import {Container} from "react-bootstrap";
+import {Table} from "react-bootstrap";
 
 const CompanyNew = () =>{
   return(
     <div id="newContainer">
-      <table>
-        <tr>
-          <td>업체 이름</td>
-          <td><input /></td>
-          <td>카테고리</td>
-          <td><input /></td>
-        </tr>
-        <tr>
-          <td>담당자</td>
-          <td><input /></td>
-          <td>지역</td>
-          <td><input /></td>
-        </tr>
-      </table>
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <td>업체 이름</td>
+            <td><input /></td>
+            <td>카테고리</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td>담당자</td>
+            <td><input /></td>
+            <td>지역</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td colSpan="4" style={{textAlign : "center"}}>
+              <button>저장</button>
+              &nbsp;&nbsp;&nbsp;
+              <button>취소</button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
