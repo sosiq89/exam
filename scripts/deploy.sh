fd2380335e2afc94c4f477cd93c4ffb7b2db6555
NamGukHyeon <sosiq89@gmail.com>
2023-09-22 17:12:33 +0900

    test 12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ sudo docker rmi -f sosiq/ppippo:latest
 sudo docker pull sosiq/ppippo:latest
 
 # 도커 run
-docker run -d -p 8123:8123 --env-file=env_list.txt --name app sosiq/ppippo:latest
+docker run -d -p 8123:8123 --name app sosiq/ppippo:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
